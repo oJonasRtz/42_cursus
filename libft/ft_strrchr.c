903399b54309fb9619886de92ba05e759d7cf729
Jonas Alberto Pereira <jopereir@c1r9p2.42sp.org.br>
2024-10-10 12:09:31 -0300

    Funções str feitas

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,7 +6,25 @@
 /*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 12:36:51 by jopereir          #+#    #+#             */
-/*   Updated: 2024/10/08 12:36:52 by jopereir         ###   ########.fr       */
+/*   Updated: 2024/10/10 12:08:51 by jopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+/*
+	Returns the last occurence of 'c' in 's'
+*/
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+
+	i = ft_strlen(s) + 1;
+	while (i > 0)
+	{
+		if (s[i - 1] == c)
+			return ((char *)&s[i - 1]);
+		i--;
+	}
+	return (NULL);
+}
--- libft/main.c
+++ libft/main.c
@@ -6,21 +6,21 @@
 /*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 12:09:27 by jopereir          #+#    #+#             */
-/*   Updated: 2024/10/10 11:44:35 by jopereir         ###   ########.fr       */
+/*   Updated: 2024/10/10 12:07:04 by jopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-int main(void)
+int	main(void)
 {
 	char	*h;
-	char	*w;
+	char	w;
 	char	*i;
 
-	w = "";
+	w = '\0';
 	h = "Hello world!!!";
-	i = ft_strnstr(h, w, ft_strlen(h));
+	i = ft_strrchr(h, w);
 	printf("Verificação: %s\n", i);
 }
