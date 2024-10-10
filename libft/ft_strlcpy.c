23e5872eb9e71aed7ce125ccc682016b9530a874
Jonas Alberto Pereira <jopereir@c1r9p2.42sp.org.br>
2024-10-10 10:26:29 -0300

    ft_strlcpy

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -1,12 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcopy.c                                      :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 12:36:43 by jopereir          #+#    #+#             */
-/*   Updated: 2024/10/08 12:36:44 by jopereir         ###   ########.fr       */
+/*   Updated: 2024/10/10 10:21:07 by jopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	length;
+
+	i = 0;
+	length = ft_strlen(src);
+	if (size)
+	{
+		while (i < size - 1 && src[i])
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (length);
+}
--- libft/main.c
+++ libft/main.c
@@ -6,7 +6,7 @@
 /*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 12:09:27 by jopereir          #+#    #+#             */
-/*   Updated: 2024/10/08 15:26:04 by jopereir         ###   ########.fr       */
+/*   Updated: 2024/10/10 10:25:11 by jopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 
 int main(void)
 {
-	char	h[] = "Hello";
+	char	h[15];
 	char	*w;
+	int		i;
 
-
-	w = " World!!";
-	ft_strlcat(h, w, 9);
-	printf("%s\n", h);
+	w = "Hello world!!";
+	i = ft_strlcpy(h, w, sizeof(h));
+	printf("ft_strcpy: %s\nTamanho: %d\n", h, i);
 }
