7c81008eb3961b1cd8519727150f1a466f884727
Jonas Alberto Pereira <jopereir@c2r1p5.42sp.org.br>
2024-10-11 10:14:26 -0300

    memmove

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,17 +6,45 @@
 /*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 12:14:38 by jopereir          #+#    #+#             */
-/*   Updated: 2024/10/10 14:41:13 by jopereir         ###   ########.fr       */
+/*   Updated: 2024/10/11 10:13:45 by jopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+	It does the same as memcpy but handles memory overlap
+	(when two variables use the same address at the same time).
+
+	When src is greater than dest, there is no overlap, so you can use memcpy().
+	Otherwise, you should write from back to front to copy correctly.
+
+	literal variables can only be read
+		ex: h = "abcdefg";
+	it must be declared as the main bellow
+
+	Main to test
+	
+#include "libft.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	char	h[] = "abcdefgh";
+	char	*w = h + 2;
+
+	ft_memmove(w, h, 9);
+	printf("Verificação: %s\n", w);
+}
+*/
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	if (src > dest)
 		return (ft_memcpy(dest, src, n));
-	while (n--)
-		((unsigned char *)dest)[n] = ((unsigned char *)src)[n];
+	else
+	{
+		while (n--)
+			((unsigned char *)dest)[n] = ((unsigned char *)src)[n];
+	}
 	return (dest);
 }
--- libft/main.c
+++ libft/main.c
@@ -6,7 +6,7 @@
 /*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 12:09:27 by jopereir          #+#    #+#             */
-/*   Updated: 2024/10/10 14:41:40 by jopereir         ###   ########.fr       */
+/*   Updated: 2024/10/11 10:07:02 by jopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,9 @@
 
 int	main(void)
 {
-	char	*h;
-	char	w[7];
+	char	h[] = "abcdefgh";
+	char	*w = h + 2;
 
-	h = "abcdefgh";
-	ft_memmove(h, h, 6);
+	ft_memmove(w, h, 9);
 	printf("Verificação: %s\n", w);
 }
