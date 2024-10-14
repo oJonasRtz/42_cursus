0d89f6b70bb4e5cb8b4bb6445cb2959ba8ed9b67
Jonas Alberto Pereira <jopereir@c2r1p5.42sp.org.br>
2024-10-14 12:49:51 -0300

    striteri test

--- libft/main.c
+++ libft/main.c
@@ -6,19 +6,22 @@
 /*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 12:09:27 by jopereir          #+#    #+#             */
-/*   Updated: 2024/10/14 12:38:10 by jopereir         ###   ########.fr       */
+/*   Updated: 2024/10/14 12:49:24 by jopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-int	main(void)
+void	print_index_char(unsigned int index, char *c)
 {
-	char	*h;
-	char	*w;
+	printf("Index: %u, Char: %c\n", index, *c);
+}
 
-	h = "hello";
-	w = ft_strmapi(h, &ft_toupper);
-	printf("%s\n", w);
+int main(void)
+{
+	char str[] = "hello";
+	
+    ft_striteri(str, print_index_char);
+    return 0;
 }
