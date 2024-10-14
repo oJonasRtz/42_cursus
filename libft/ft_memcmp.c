805997ed53495db0f4690bb7a777e01f2aa802b6
Jonas Alberto Pereira <jopereir@c2r1p5.42sp.org.br>
2024-10-14 13:45:14 -0300

    memcmp recovery

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 12:14:29 by jopereir          #+#    #+#             */
-/*   Updated: 2024/10/10 14:12:22 by jopereir         ###   ########.fr       */
+/*   Updated: 2024/10/14 13:44:23 by jopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,3 +18,20 @@
 	returns 0 if there equal
 	returns the diference between then if there's a diference
 */
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*pnt1;
+	unsigned char	*pnt2;
+
+	i = 0;
+	pnt1 = (unsigned char *)s1;
+	pnt2 = (unsigned char *)s2;
+	while (i < n)
+	{
+		if (pnt1[i] != pnt2[i])
+			return (pnt1[i] - pnt2[i]);
+		i++;
+	}
+	return (0);
+}
