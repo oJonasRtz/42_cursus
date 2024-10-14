4fbe28205d10af76de388bd562caaf5fd456a5cd
Jonas Alberto Pereira <jopereir@c2r1p5.42sp.org.br>
2024-10-14 12:48:19 -0300

    striteri

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -6,7 +6,20 @@
 /*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 13:11:54 by jopereir          #+#    #+#             */
-/*   Updated: 2024/10/11 13:11:55 by jopereir         ###   ########.fr       */
+/*   Updated: 2024/10/14 12:47:26 by jopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		f(i, &s[i]);
+		i++;
+	}
+}
