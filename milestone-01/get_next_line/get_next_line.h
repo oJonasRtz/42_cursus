ed1a4f0752f89cac35abe637817bc8ce9af61bc0
Jonas Alberto Pereira <jopereir@c3r6p1.42sp.org.br>
2024-10-15 11:37:46 -0300

    milestone-01

---
+++ milestone-01/get_next_line/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/15 11:33:52 by jopereir          #+#    #+#             */
+/*   Updated: 2024/10/15 11:36:00 by jopereir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
