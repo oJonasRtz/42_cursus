cbbcbf234083b4720b97626d38ed98bf4f42bb27
Jonas Alberto Pereira <jopereir@c2r1p5.42sp.org.br>
2024-10-24 16:36:23 -0300

    ...

--- M1/ft_printf/ft_printf_hex.c
+++ M1/ft_printf/ft_printf_hex.c
@@ -1,29 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_printf_hex.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/18 11:30:52 by jopereir          #+#    #+#             */
-/*   Updated: 2024/10/24 16:34:34 by jopereir         ###   ########.fr       */
+/*   Created: 2024/10/24 16:34:18 by jopereir          #+#    #+#             */
+/*   Updated: 2024/10/24 16:35:09 by jopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "./include/ft_printf.h"
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <stdarg.h>
-# include "libft.h"
 
-int	ft_printf(const char *str, ...);
-int	ft_num_count(int n);
-int	ft_get_unsigned(unsigned int n);
-int	ft_get_int(int n);
-int	ft_get_percentage(void);
-int	ft_get_char(char c);
-int	ft_get_string(char *str);
-
-#endif
